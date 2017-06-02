da3adca6e0f36305bbb797e96734240d776622f3
onkbear <onkbear@gmail.com>
2017-06-02 20:18:18 +0900

    Add editor

---
+++ src/components/Editor.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="editor">
+    <textarea :value="input" @input="update"></textarea>
+    <div v-html="compiledMarkdown"></div>
+  </div>
+</template>
+
+<script>
+import marked from 'marked'
+import 'lodash'
+export default {
+  name: 'editor',
+  data () {
+    return {
+      input: '# hello'
+    }
+  },
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.input, { sanitize: true })
+    }
+  },
+  methods: {
+    update: _.debounce(function (e) {
+      this.input = e.target.value
+    }, 300)
+  }
+}
+</script>
