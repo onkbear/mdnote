3ec045c763a3e8e07311193cb1213d6f4d6cb917
onkbear <onkbear@gmail.com>
2017-06-07 20:25:34 +0900

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <navbar></navbar>
     <editor></editor>
   </div>
 </template>
 
 <script>
 import Editor from './components/Editor'
+import Navbar from './components/Navbar'
 
 export default {
   name: 'app',
   components: {
-    Editor
+    Editor,
+    Navbar
   }
 }
 </script>
